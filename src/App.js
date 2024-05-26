a132944655c1fb95bb905c1813d5df1c1f18d7a9
tanyaporseva <tporseva@inbox.ru>
2024-05-26 14:22:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: "1", title: "Пост", body: "это пост хихихи"},
+    {id: "2", title: "Пост", body: "это пост хихихи"},
+    {id: "3", title: "Пост", body: "это пост хихихи"},
+    {id: "4", title: "Пост", body: "это пост хихихи"}
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}/>
+      <PostList remove={removePost} posts={posts} title={"Список постов"}/>
     </div>
   );
 }
